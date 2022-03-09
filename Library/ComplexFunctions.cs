e36ab31cd0eff385dffb9169f26b73d1fba886b5
blizzardisdead <94704652+blizzardisdead@users.noreply.github.com>
2022-03-09 13:04:37 +0300

    1

---
+++ Library/ComplexFunctions.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+
+namespace Library
+{/// <summary>
+/// Класс содержит два метода построения чисел и преобразование последовательности чисел.
+/// </summary>
+    public class ComplexFunctions
+    {
+        /// <summary>
+        /// Данный метод выводит последовательность положительных чисел от 1 до параметра N.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public string UseNumber(uint n)
+        {
+            string number = "";
+            var i = 1;
+            
+            if (n == 0)
+            {
+                throw new Exception("Введенное значение должно быть больше 0");
+            }
+
+            while(i < n)
+            {
+                number += ((i++) + ", ");
+            }
+            number +=n;
+            return number;
+           
+        }
+     /// <summary>
+     /// Данный метод рисует квадрат со стороной N и пустым значением внутри.
+     /// </summary>
+     /// <param name="n"></param>
+
+         public string UseSquare(uint n)
+         {
+            string square = "";
+            var mid = n / 2;
+
+            if (n % 2 == 0)
+            {
+                throw new Exception("Введите нечётное число");
+            }
+
+            for (var i = 0; i <n ; i++)
+            {
+                for (var j = 0; j < n; j++)
+                {
+
+                    if (j == mid && i == mid)
+                    {
+                        square+=" ";
+                    }
+                    else
+                    {
+                        square+="*";
+                    }
+
+                }
+                 square+='\n';
+                
+            }
+            return square;
+        }
+
+    }
+
+}
--- NumberSquare/ProgramConsole.cs
+++ NumberSquare/ProgramConsole.cs
@@ -4,24 +4,27 @@ using Library;
 namespace NumberSquare
 {
     class ProgramConsole
-    {
+    {/// <summary>
+    /// Принимает от пользователя параметр N и использует библиотеку классов.
+    /// </summary>
+    /// <param name="args"></param>
         static void Main(string[] args)
         {
-
             Console.WriteLine("Введите положительный параметр N");
+   
             try
             {
                 uint n = uint.Parse(Console.ReadLine());
                 ComplexFunctions function = new ComplexFunctions();
                 Console.WriteLine(function.UseNumber(n));
-                function.UseSquare(n);
+                Console.WriteLine(function.UseSquare(n));
+
 
             }
             catch
             {
-                Console.WriteLine("Пожалуйста, введите положительный параметр!");
+                Console.WriteLine("Введён не соответствующий параметр");
             }
-            // Принимает от пользователя параметр N и использует библиотеку классов.
             
         }
     }
